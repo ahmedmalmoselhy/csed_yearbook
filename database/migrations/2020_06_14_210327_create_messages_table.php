c7a34a6184eaadaa592555424470ec2114b3a2dd
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2020-06-14 23:09:19 +0200

    created db migration

---
+++ database/migrations/2020_06_14_210327_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("from_id")->nullable(true);
+            $table->unsignedBigInteger("to_id")->nullable(true);
+            $table->string("message");
+            $table->boolean("is_known");
+            $table->boolean("is_visible");
+            $table->boolean("is_public");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
