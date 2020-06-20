0d676fe560ecca87f242fd950bd15c92be254518
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2020-06-20 19:55:17 +0200

    show all public posts and send them to home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,5 +9,30 @@ use App\Message;
 
 class HomeController extends Controller
 {
+    public function showHome(){
+        $messages_response = [];
+        $messages = Message::where('is_public', 1)->get();
+        if($messages -> isNotEmpty()){
+            foreach($messages as $message){
+                // get the message data
+                $text = $message -> message;
+                $timestamp = $message -> created_at;
+                $sender_name = null;
+                if($message -> is_known == 1){
+                    $sender = User::where('id', $message -> from_id)->first();
+                    if($sender != null){
+                        $sender_name = $sender -> full_name;
+                    }
+                }
+                $message_response = [
+                    'from' => $sender_name,
+                    'message' => $text,
+                    'time' => $timestamp
+                ];
+                $messages_response[] = $message_response;
+            }
+        }
 
+        return view('home')->with('messages_response', $messages_response);
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+@foreach ($messages_response as $item)
+    <h1> {{ $item['from'] }} </h1>
+@endforeach
